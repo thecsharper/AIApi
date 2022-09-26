1fe0db1eca39093e4b36f906e52716523d0fd029
thecsharper <si@thecsharper.com>
2022-09-26 17:20:56 +0100

    Refactor and renamed files.

---
+++ AIApi.Messages/ImageClassifierEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using AIApi.Events;
+
+namespace AIApi.Messages
+{
+    public record ImageClassifierEvent : IntegrationEvent
+    {
+        public string ImageReference { get; set; }
+        
+        public string Description { get; set; }
+
+        public Guid MessageId { get; set; }
+
+        public ImageClassifierEvent(string imageReference, string description, Guid messageId)
+        {
+            ImageReference = imageReference;
+            Description = description;
+            MessageId = messageId;
+        }
+    }
+}
